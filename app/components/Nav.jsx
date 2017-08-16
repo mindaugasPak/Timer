49c26301beeecd45a66539e45c16be3a169f6f66
MindaugasPak <mindaugas.pak@gmail.com>
2017-08-16 22:15:34 +0200

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router')
+
+
+var Nav = () => {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Timer</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>CountDown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by Mindaugas Pakalniskis
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+module.exports = Nav;
